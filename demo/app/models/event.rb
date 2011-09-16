d31c9053ba7f770c5b96525b33b1e8c8ba61951d
Wen-Tien Chang <ihower@gmail.com>
2011-09-16 23:49:00 +0800

    Add Attendee, Group, Location, EventGroupship models

--- demo/app/models/event.rb
+++ demo/app/models/event.rb
@@ -1,3 +1,13 @@
 class Event < ActiveRecord::Base
   validates_presence_of :name
+  
+  belongs_to :category
+  
+  has_one :location
+  
+  has_many :attendees
+  
+  has_many :event_groupships
+  has_many :groups, :through => :event_groupships
+   
 end
--- demo/db/schema.rb
+++ demo/db/schema.rb
@@ -11,11 +11,26 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110916150528) do
+ActiveRecord::Schema.define(:version => 20110916153328) do
+
+  create_table "attendees", :force => true do |t|
+    t.string   "name"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "categories", :force => true do |t|
     t.string   "name"
     t.integer  "position"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "description"
+  end
+
+  create_table "event_groupships", :force => true do |t|
+    t.integer  "event_id"
+    t.integer  "group_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -32,6 +47,19 @@ ActiveRecord::Schema.define(:version => 20110916150528) do
   end
 
   add_index "events", ["category_id"], :name => "index_events_on_category_id"
+
+  create_table "groups", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "locations", :force => true do |t|
+    t.string   "name"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "people", :force => true do |t|
     t.string   "name"
