c14fb75889ae6fde9fd411d5bff2145b85252cac
Wen-Tien Chang <ihower@gmail.com>
2011-09-16 16:28:21 +0800

    firststep and welcome

---
+++ demo/app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+class WelcomeController < ApplicationController
+  
+  def index
+  end
+  
+  def say
+  end
+  
+end
--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -1,4 +1,8 @@
 Demo::Application.routes.draw do
+  
+  get "welcome" => "welcome#index"
+  get "welcome/say_hello" => "welcome#say"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ Demo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
