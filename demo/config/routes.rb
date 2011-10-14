fb1efd72bd5c926f9231c95d7e02177ce7406d9c
Wen-Tien Chang <ihower@gmail.com>
2011-10-14 22:44:49 +0800

    Install devise

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -1,5 +1,7 @@
 Demo::Application.routes.draw do
   
+  devise_for :users
+
   resources :events do
     resource :state, :controller => 'event_states'
     resource :location, :controller => 'event_locations'
