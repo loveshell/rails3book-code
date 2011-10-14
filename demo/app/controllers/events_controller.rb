47fff8aa114ba46c0efa70ba866c7ee381b28fd4
Wen-Tien Chang <ihower@gmail.com>
2011-10-14 22:47:01 +0800

    Add authenticate_user

--- demo/app/controllers/events_controller.rb
+++ demo/app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 class EventsController < ApplicationController
   
+  before_filter :authenticate_user!
   before_filter :find_event, :only => [ :show, :edit, :update, :destroy]
   
   def index
